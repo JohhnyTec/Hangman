df176849044e8c54d1c97608f190eee9fd4a9547
JohnnyTec <johnny.hoffmann@tn.techstarter.de>
2025-01-30 14:31:12 +0100

    debugged

--- basic_server.js
+++ basic_server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url');
 const hostname = '127.0.0.1';
 const port = 3000;
+
 const server = http.createServer((req, res) => {
   const queryObject = url.parse(req.url,true).query;
   let result = parseInt(queryObject.a) + parseInt(queryObject.b);
@@ -9,6 +10,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end(`Result: ${result}\n`);
 });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
